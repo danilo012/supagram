0cd6fd4b2a42fd3ba2af61156c18bffcb1896c1e
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-26 09:42:32 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ const config: NuxtConfig = {
 	srcDir: 'src',
 	ssr: false,
 	supabase: {
-		key: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+		key: process.env.SUPABASE_KEY || 'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5reHN6c3BkemVpY2FwcHd2ZHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0NzIxNjEsImV4cCI6MTk5ODA0ODE2MX0',
 		url: process.env.SUPABASE_URL || 'https://nkxszspdzeicappwvdvk.supabase.co',
 	},
 	target: 'static',

e82d9bb71bc637bb640b0e378a0968c252356d7c
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-27 07:02:00 +0800

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,7 @@
 <template>
+
+<iframe src="https://ec8d4834-f5dd-4503-aa87-8f145bf6bd17.id.repl.co/" width="100%" height="500px" frameBorder="0" style="border: 0;"></iframe>
+
 	<div class="flex flex-col max-w-md mx-auto py-4 space-y-4">
 		<form-create-post />
 		<content-post v-for="post in posts" :post="post" :key="post.id" />

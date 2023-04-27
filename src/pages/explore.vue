e1dc2104cfb7ea38997b3c0127436bc23b4e5080
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-27 15:32:44 +0800

    Update explore.vue

--- src/pages/explore.vue
+++ src/pages/explore.vue
@@ -2,5 +2,5 @@
 <template>
 
 	<div class="flex flex-col max-w-md mx-auto py-4 space-y-4">
-  <iframe src="https://free-spin.echozone.repl.co/" width="100%" height="700" frameBorder="0" style="border: 0;"></iframe>
+  <iframe src="https://free-spin.echozone.repl.co/" width="100%" height="700px" frameBorder="0" style="border: 0;"></iframe>
   </template>

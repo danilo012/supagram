91bf4c8333de9999c7a58e5487051caa6a13aefc
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-27 15:23:57 +0800

    Update and rename explore to explore.vue

---
+++ src/pages/explore.vue
@@ -0,0 +1,6 @@
+
+<template>
+
+	<div class="flex flex-col max-w-md mx-auto py-4 space-y-4">
+  <div><iframe src="https://free-spin.echozone.repl.co/" width="100%" height="100px" frameBorder="0" style="border: 0;"></iframe></div>
+  </template>

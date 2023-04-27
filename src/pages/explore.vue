050d6b3d54b2e310178031ff399f6192b0db92f4
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-27 15:41:02 +0800

    Update explore.vue

--- src/pages/explore.vue
+++ src/pages/explore.vue
@@ -1,6 +1,31 @@
+<!DOCTYPE html>
+<html>  
+<head>
+  <script id='pixel-script-poptin' src='https://cdn.popt.in/pixel.js?id=c7d5f4e64f843' async='true'></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="styles1.css" rel="stylesheet" type="text/css" />
+<body> 
+<!--spin id  #c7d5f4e64f843 ---->
+<!--mystery id #94d69eb07c839  --><!--scratch id  ---->
+  <h> REWARDS SCHEDULE:
+  <br>
+  FEEE SPIN (DAILY) - 6:00AM to 9:00AM
+  <br>
+  FREE USD (DAILY)- 12:00AM to 3:00PM
+  <br>
+  FREE SCRATCH IT (DAILY)- COMING SOON 
+  <br>
+  <mark>COMEBACK EVERYDAY FOR MORE CHANCES OF WINNING</mark></h>
+  <marquee><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiGRskkOtko_aZKnn7ngwM6hjKBMGYhIe9s1_S5x46855l-MMBXGIvN70vwXRySVmWDrZfX1LuisZgHg-PLqZyKBxpdJ_OG5YYzQrg4CC9ECFMCcrSjTxlkQIhidV9V_TdmJaBd6NDQ_TrMPQ8yqFL95mtrsQRmjZZecGRvF9I997dEt82j74XYO9U67w/s225/IMG_20230419_133458.jpg" 
+   alt="" width="100%" height="700px" style="border:1.9px solid black">
+  <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgmpAFPFf3cHIO1_OnaUT0lQWVxYcKf1ICVsdDRZGJwYTiZ1bSAPyFjGPNbAtO3gXu8SzwxFzEGDiboVcqOHxh17D-C0Rjuc0-xQ__GC6D4w5i2UUbeS1tXDqT0dT5lCPmHAP2AyBisQQxXhzr8yhjwZNBDzzDKiKsGZwzBkq9s5dkTxcQQd5tgZuoTmg/s2049/IMG_20230418_104535.jpg" 
+   alt="" width="100%" height="700px" style="border:1.9px solid black">
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiGRskkOtko_aZKnn7ngwM6hjKBMGYhIe9s1_S5x46855l-MMBXGIvN70vwXRySVmWDrZfX1LuisZgHg-PLqZyKBxpdJ_OG5YYzQrg4CC9ECFMCcrSjTxlkQIhidV9V_TdmJaBd6NDQ_TrMPQ8yqFL95mtrsQRmjZZecGRvF9I997dEt82j74XYO9U67w/s225/IMG_20230419_133458.jpg" 
+   alt="" width="100%" height="700px" style="border:1.9px solid black">
+   </marquee>
+   </body>
+</html>
 
-<template>
 
-	<div class="flex flex-col max-w-md mx-auto py-4 space-y-4">
-  <iframe src="https://free-spin.echozone.repl.co/" width="100%" height="700px" frameBorder="0" style="border: 0;"></iframe>
-  </template>

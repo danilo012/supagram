67e8fdf445595951a885524555f4644ffd73de14
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-05-16 20:01:40 +0800

    Delete unauthenticated.ts

--- src/middleware/unauthenticated.ts
+++
@@ -1,9 +0,0 @@
-import { Middleware } from '@nuxt/types'
-
-const middleware: Middleware = ({ store, redirect }) => {
-	if (store.state.auth.user) {
-		redirect('/')
-	}
-}
-
-export default middleware
